8ee455f38fa1ca04f0047440180ca2a91b1c191d
raoufffffff <112146388+raoufffffff@users.noreply.github.com>
2024-12-12 02:16:22 +0100

    Add files via upload

---
+++ src/compunent/CardItems/CardaiatemCard.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import { CiCircleMinus, CiCirclePlus } from 'react-icons/ci'
+import { useSnapshot } from 'valtio'
+import state from '../../stor/stor'
+import { motion } from 'motion/react'
+const CardaiatemCard = ({e, i}) => {
+    const snap = useSnapshot(state)
+    const [animate, setAnimate] = useState(false);
+
+    const handleAnimation = () => {
+      setAnimate(true);
+      setTimeout(() => setAnimate(false), 1000);
+    };
+    const add = (a)=>{
+        state.items = state.items.map((e) =>
+            e.name === a.name ? {...e, q: e.q + 1 } : e
+          )
+          handleAnimation()
+    }
+    const re = (a)=>{
+        if (a.q > 1) {
+            state.items = state.items.map((e) =>
+              e.name === a.name ? { ...e, q: e.q - 1 } : e
+            );
+            handleAnimation()
+          } else {
+            // Remove the item entirely if quantity reaches 0
+            state.items = state.items.filter((e) => e.name !== a.name);
+          }
+    }
+  return (
+    <motion.div
+    initial={{x: -1000}}
+    animate={{x: 0}}
+    transition={{delay: 0.2 + ((i / 10 + 0.1) * 2), duration: 0.3, type: "spring"}}
+    className='w-full md:w-5/12 flex mb-3 pb-1 relative'
+    >
+        <div
+        className='w-3/12 relative'
+        >
+            <motion.span
+             animate={animate ? { scale: [1, 1.25, 0.75, 1] } : {}}
+             transition={{ duration: 1 }}
+            className='absolute bg-[#3e61ae] text-white py-1 px-3 rounded-full -top-2 left-0'
+            >{e.q}</motion.span>
+            <img 
+            src={e.img}
+            />
+        </div>
+       
+        <div
+        className='w-6/12 flex flex-col'
+        >
+            <span
+            >{e.name}</span>
+            <div
+            className='flex mt-auto pb-2'
+            >
+<span
+className={`font-bold ${e.offer && "line-through text-gray-400 font-normal"}`}
+>{e.price} DA</span>
+{e.offer && <span
+className='font-bold ml-3'
+>{e.newprice} DA</span>}
+            </div>
+        </div>
+        <div
+        className='w-3/12 flex flex-col items-end pr-3 py-1 justify-between'
+        >
+            <CiCirclePlus 
+            onClick={()=>{
+                add(e)
+            }}
+            size={30}
+            className='text-green-600'
+            />
+            <CiCircleMinus 
+            onClick={()=>{
+               re(e)
+            }}
+            size={30}
+            className='text-red-600'
+            />
+
+        </div>
+        
+       <div
+       className='absolute bottom-0 left-[50%] translate-x-[-50%] w-11/12 h-[0.5px] bg-gray-600'
+       ></div>
+    </motion.div>
+  )
+}
+
+export default CardaiatemCard
--- src/pages/Card/Card.jsx
+++ src/pages/Card/Card.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
 import { useSnapshot } from 'valtio'
 import state from '../../stor/stor'
-
+import { motion } from 'motion/react'
+import { Link } from 'react-router-dom'
+import CardaiatemCard from '../../compunent/CardItems/CardaiatemCard'
 const Card = () => {
+    const time = new Date().getHours()
     const snap = useSnapshot(state)
+    const price = ()=>{
+        let a = 0
+        snap.items.map(e => {
+            a += (e.q * (e.offer ? e.newprice : e.price))
+        })
+        return a
+    }
+   
+    const ourCardItems = state.items.map((e, i)=>{
+        return <CardaiatemCard e={e} key={i} i={i}/>
+    })
   return (
-    <div>Card</div>
+    <motion.div
+    initial={{scale: 0}}
+    animate={{scale: 1}}
+    transition={{duration: 0.5, type: "spring"}}
+    className='min-h-screen'
+    >
+<h1
+className='text-center md:text-start px-5 font-bold text-2xl my-2'
+>Card</h1>
+<div
+className='flex flex-col mt-4 px-3 md:flex-row md:flex-wrap md:justify-center'
+>
+    {ourCardItems}
+</div>
+<div
+className={`w-full border-t   border-t-gray-500 ${snap.items.length === 1 ? "mt-48" : snap.items.length === 2 ? "mt-28" : "mt-10"  } relative`}
+>
+    <div
+    className='px-4 w-fit absolute -top-[15%] md:-top-[10%] z-50  rounded-lg bg-[#dd2a5b] text-white text-center left-[50%] translate-x-[-50%] border border-gray-500'
+    >
+        time of delevry {time > 17 ? "tomorrow evning" : "today evning"}
+    </div>
+    <div
+    className='text-xl md:text-3xl md:text-center font-[600] px-5 mt-10'
+    >
+        total : <span
+        className='font-bold'
+        >{price()} DA</span>
+    </div>
+    <p
+    className='my-5 text-center text-[#777]'
+    >some prices may change</p>
+    <Link
+    className='w-9/12 flex mb-5 rounded-2xl justify-center py-2 mx-auto md:w-6/12 bg-[#dd2a5b] text-white'
+    to={'/checkout'}
+    >next</Link>
+</div>
+    </motion.div>
   )
 }
 
